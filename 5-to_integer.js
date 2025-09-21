7226810afb78a9688e7d19f5287a1bd7605b6d2c
Philopatir Rafik <filorm7@gmail.com>
2025-09-21 12:05:12 +0300

    task 5 update

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,7 +1,8 @@
-let arg = process.agrv[2];
-if(arg === undefined || isNaN(Number(arg))){
-    console.log("Not a number");
-}
-else{
-    console.log(`My number: ${arg}`);
+let arg = process.argv[2];
+let num = parseInt(arg);
+
+if (isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + num);
 }
